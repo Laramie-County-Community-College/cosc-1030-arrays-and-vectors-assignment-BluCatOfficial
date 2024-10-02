5066084bcffba06529cfc60a17eb225d0a964988
BluCatOfficial <jaelyntafoya@student.lccc.wy.edu>
2024-10-02 22:15:45 +0000

    Changes to be committed: 	new file:   Arrays_Vectors 	modified:   Arrays_Vectors.cpp

--- Arrays_Vectors.cpp
+++ Arrays_Vectors.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <string>
 using namespace std;
-
+//We're back to where the output is 3 numbers
 
 int main() {
+// lets use vectors
+std::vector<int> numbers(3);
+cout << "Enter three integers: ";
+for (int i = 0; i < 3; i++) {
 
-std::vector<int> intvector = [12, 7, 9];
+    cin >> numbers[i];
+}
+// Now to find the smallest number
+int smallest = numbers[0];
 
+for (int i = 1; i < 3; i++) {
+    if (numbers[i]< smallest) {
+        smallest = numbers [i];
+    }
+}
 
+// Now the output
+cout << "The smallest of the three is..." << smallest << endl;
 
 }
